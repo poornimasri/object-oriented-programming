ceaede876864ed8dc4037c59cc1cf9802c96b99f
poornimasri <43128493+poornimasri@users.noreply.github.com>
2026-06-07 14:42:13 +0530

    Add files via upload

---
+++ allaboutcountries.py
@@ -0,0 +1,30 @@
+class India():
+    def capital(self):
+        print("New delhi is the capital of india")
+
+    def language(self):
+        print("Hindi is the most widely spoken language of India")
+
+    def type(self):
+        print("India is a developing country")  
+
+
+class USA():
+    def capital(self):
+        print("Washington DC is the capital of USA")
+
+    def language(self):
+        print("English is the primary language of USA")
+
+    def type(self):
+        print("USA is a developed country")  
+
+object1=India()
+object1.capital()
+object1.language()
+object1.type()
+
+object2=USA()
+object2.capital()
+object2.language()
+object2.type()
